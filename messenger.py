d84ecbc78db71c3a66357b4140db432d39e23293
codewithyasho <141209391+codewithyasho@users.noreply.github.com>
2024-02-18 23:56:21 +0530

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -9,14 +9,14 @@ def main():
     counts = st.number_input('How many times do you want to send the message?', min_value=1, step=1, value=1)
 
     if st.button('Start Sending'):
-        st.write("This program will run for 10 seconds...")
+        st.write("This program will run in 10 seconds...")
         time.sleep(10)
         message_count = counts
         while message_count > 0:
             pyautogui.typewrite(your_message)
             pyautogui.press("enter")
             message_count -= 1
-            st.write(f"Message sent, {message_count} messages left.")
+            st.write(f"Message sent successfully!, {message_count} messages left.")
 
 if __name__ == "__main__":
     main()
